8baefcafab8ede47c3a7dc7aa41fd4f5e396de01
Ryan Stenersen <143983140+ryansten20@users.noreply.github.com>
2025-10-06 13:28:09 -0700

    Added Login and Register page

--- miniMarket-frontend/src/pages/Register.jsx
+++ miniMarket-frontend/src/pages/Register.jsx
@@ -1,8 +1,47 @@
+import { Link } from "react-router-dom";
+import "./Login.css";
+import React, {useState} from "react";
+import logo from "../assets/mini.png";
+
 export default function Register() {
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirm: "",
+        agree: false,
+    });
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setForm((prev) => ({
+            ...prev,
+            [name]: type === "checkbox" ? checked : value,
+        }));
+    };
+
+    const sendRegister = (e) => {
+        e.preventDefault();
+        console.log(form.name);
+        console.log(form.email);
+        console.log(form.password);
+        console.log(form.confirm);
+
+    }
     return (
-        <div>
-            <h1 className="text-2xl font-bold">Register Page</h1>
-            <p className="mt-2 text-gray-600">Registration form will go here.</p>
+        <div className="login-page">
+            <img src={logo} alt="Logo" className= "logo" />
+            <h1>Register</h1>
+            <form onSubmit={sendRegister}>
+                <input type="text" name = "name" className="name" placeholder="Name" value = {form.name} onChange={(handleChange)} required/>
+                <input type="email" name = "email" className="email" placeholder="E-mail" value={form.email} onChange={(handleChange)} required/>
+                <input type="password" name="password" className="password" placeholder="Password" value={form.password} onChange={(handleChange)} required/>
+                <input type="password" name="confirm" className="confirm" placeholder="Confirm" value={form.confirm} onChange={(handleChange)} required/>
+                <label>
+                <input type="checkbox" name = "agree" className="agree" placeholder="Confirm" value={form.agree} onChange={(handleChange)} required/>
+                I agree to the terms and conditions.
+                </label>
+                <button type="submit">Register</button>
+            </form>
         </div>
     );
 }
